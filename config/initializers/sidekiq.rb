f95bcd903a04c0ef1b5f2dddd3be21967523de3e
yazan9 <yazan.khalaileh@gmail.com>
2023-09-05 13:04:19 -0700

    added redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
 end
