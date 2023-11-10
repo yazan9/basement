7394fe9406942f7f4c91b28ac73b580ffbc74d90
yazan9 <yazan.khalaileh@gmail.com>
2023-11-10 15:14:18 -0800

    update confirmation token

--- app/mailers/accounts/account_mailer.rb
+++ app/mailers/accounts/account_mailer.rb
@@ -3,7 +3,7 @@ class Accounts::AccountMailer < ApplicationMailer
   def send_email_confirmation(user)
     @user = user
     @encoded_image = Base64.encode64(File.binread(Rails.root.join("public/logo.png"))).gsub("\n", '')
-    @link = "#{ENV.fetch('PIXIES_UI_URL', 'https://helipingpixies.com')}/do-confirm-email?token=#{user.confirmation_token}"
+    @link = "#{ENV.fetch('PIXIES_UI_URL', 'https://helpingpixies.com')}/do-confirm-email?confirmation_token=#{user.confirmation_token}"
     mail(to: user.email, subject: 'Welcome to Helping Pixies! Please confirm your email address by clicking the link below')
   end
 end
